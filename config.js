11956d25b0f47d80f7d30b3cad3363d70ce18777
miljkoyu <miljkoyu@live.com>
2022-03-07 23:39:48 +0100

    24. Difficulty and the Nonce Value | Code

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
+//broj 0 na pocetku hash vrednosti
+const INITIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash: '-----',
     hash: 'hash-one',
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
     data: []
 };
 
